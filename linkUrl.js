fc162b8a5bc073f9cfb6344c9aa146e2cbedb1bd
steven-001 <68858713+steven-001@users.noreply.github.com>
2021-09-07 00:03:36 +0800

    Add files via upload

--- linkUrl.js
+++ linkUrl.js
@@ -1,17 +1,21 @@
-var link_arr = [
-      {
-        "linkName": '线路1',
-        "linkUrl": 'https://h5.qyhyin1.com/',
-        "isVip": 0
-      },
-      {
-        "linkName": '线路2',
-        "linkUrl": 'https://h5.qyhyin2.com/',
-        "isVip": 0
-      },
-      {
-        "linkName": '线路3',
-        "linkUrl": 'https://h5.qyhyin3.com/',
-        "isVip": 0
-      }
-    ],
+let link_arr = [
+  {
+    "linkName": '线路1',
+    "linkUrl": 'https://h5.qyhyin1.com/',
+    "isVip": 0
+  },
+  {
+    "linkName": '线路2',
+    "linkUrl": 'https://h5.qyhyin2.com/',
+    "isVip": 0
+  },
+  {
+    "linkName": '线路3',
+    "linkUrl": 'https://h5.qyhyin3.com/',
+    "isVip": 0
+  }
+]
+function getLinkArr() {
+  return link_arr
+}
+
